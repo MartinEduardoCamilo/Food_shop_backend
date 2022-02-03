e692e3f5841e56b13c1bc80a75d78c1dbefe3184
MartinEduardoCamilo <martin_camilo@ucne.edu.do>
2022-02-03 11:40:19 -0400

    Preparando la conexión de la base de datos finalizada

---
+++ Food_shop_backend/Context/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Food_shop_backend.Context
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+    }
+}
